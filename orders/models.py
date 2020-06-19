5e4cf4a7ae9e9467a57d6f4c19565ea0bad45957
SaintHuddini <Salah1799@hotmail.com>
2020-06-19 22:16:46 +0000

    template orders folder created with homepage and menu selection page. Food selection added to database through django shell.

--- orders/models.py
+++ orders/models.py
@@ -17,7 +17,7 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name          = models.CharField(max_length=32)
-    ingredients   = models.ManyToManyField(Ingredient)
+    description   = models.TextField(max_length=124, blank=True)
     category      = models.CharField(max_length=32, default='PIZZA', choices= (('PIZZA', 'Pizza'), ('HAMBURGER', 'Hamburger'), ('SALAD', 'Salad'), ('PASTA', 'Pasta'), ('KEBAB', 'Kebab')))
     price         = models.DecimalField(max_digits=5, decimal_places=2)
 
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Recipe
 # Create your views here.
+
+pizza = Recipe.objects.filter(category='PIZZA')
+hamburger = Recipe.objects.filter(category='HAMBURGER')
+pasta = Recipe.objects.filter(category='PASTA')
+salad = Recipe.objects.filter(category='SALAD')
+kebab = Recipe.objects.filter(category='KEBAB')
+
+def home(request):
+    return render(request, 'orders/home.html')
+
+def menu(request):
+    context = {
+        'pizza': pizza,
+        'kebab': kebab,
+        'pasta': pasta,
+        'salad': salad,
+        'hamburger': hamburger,
+    }
+    return render(request, 'orders/menu.html', context)
