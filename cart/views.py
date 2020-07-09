2ac553e883d9e21708f2406749d9421f3e16891b
SaintHuddini <Salah1799@hotmail.com>
2020-07-09 18:56:48 +0000

    Editing/Updating items in the cart is working now

--- cart/views.py
+++ cart/views.py
@@ -5,9 +5,18 @@ from .cart import Cart
 from .forms import CartAddFoodForm
 from django.http import HttpResponse
 # Create your views here.
+
+
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddFoodForm(initial={
+            'quantity': item['quantity'],
+            'override': True
+        })
+    
     return render(request, 'cart/detail.html', {'cart': cart})
+
 
 @require_POST
 def cart_add(request, food_id):
@@ -16,7 +25,8 @@ def cart_add(request, food_id):
     form = CartAddFoodForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(food=food, quantity=cd['quantity'], override_quantity=cd['override'])
+        cart.add(food=food, quantity=cd['quantity'],
+                 override_quantity=cd['override'])
         return redirect('cart:cart_detail')
 
 
@@ -27,6 +37,3 @@ def cart_remove(request, id):
     cart.cart.pop(id)
     cart.save()
     return redirect('cart:cart_detail')
-
-
-
