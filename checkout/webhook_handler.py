c29f655953d2eeb21224d265dd3d0c6a119cfac3
SaintHuddini <Salah1799@hotmail.com>
2020-07-20 21:34:42 +0000

    completed confirmation emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -6,6 +6,9 @@ from orders.models import Recipe
 import json
 import time
 from profiles.models import UserProfile
+from django.template.loader import render_to_string
+from pizza_house import settings
+from django.core.mail import send_mail
 
 
 class StripeWH_Handler:
@@ -13,6 +16,25 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )        
+
 
     def handle_event(self, event):
         """
@@ -79,6 +101,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -113,6 +136,8 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
