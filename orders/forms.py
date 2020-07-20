4ae47f5161866630b16b28df5c1e9107a37c50c8
SaintHuddini <Salah1799@hotmail.com>
2020-07-20 21:44:24 +0000

    FoodForm created for Product Admin

--- orders/forms.py
+++ orders/forms.py
@@ -1,7 +1,16 @@
 from django import forms
-from .models import Recipe, Ingredient, OrderItem
+from .models import Recipe, Category
+
 
 class FoodForm(forms.ModelForm):
+
     class Meta:
-        model = OrderItem
-        fields = ['foods', 'extra', ]
+        model = Recipe
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+      
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
