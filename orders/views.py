6b36be07f6e630fd13d59f1166202d2e1737156b
SaintHuddini <Salah1799@hotmail.com>
2020-07-21 14:28:01 +0000

    Adding new food to the menu function added

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .models import Recipe
 from django.http import HttpResponse
 from .forms import FoodForm
+from django.contrib import messages
+from django.urls import reverse
 # Create your views here.
 
 pizza = Recipe.objects.filter(category='PIZZA')
@@ -10,8 +12,10 @@ pasta = Recipe.objects.filter(category='PASTA')
 salad = Recipe.objects.filter(category='SALAD')
 kebab = Recipe.objects.filter(category='KEBAB')
 
+
 def home(request):
     return render(request, 'orders/home.html')
+
 
 def menu(request):
     context = {
@@ -22,6 +26,7 @@ def menu(request):
         'hamburger': hamburger,
     }
     return render(request, 'orders/menu.html', context)
+
 
 def pizza_menu(request):
     context = {
@@ -36,11 +41,13 @@ def kebab_menu(request):
     }
     return render(request, 'orders/kebab.html', context)
 
+
 def salad_menu(request):
     context = {
         'salad': salad,
     }
     return render(request, 'orders/salad.html', context)
+
 
 def pasta_menu(request):
     context = {
@@ -48,15 +55,28 @@ def pasta_menu(request):
     }
     return render(request, 'orders/pasta.html', context)
 
+
 def hamburger_menu(request):
     context = {
         'hamburger': hamburger,
     }
     return render(request, 'orders/hamburger.html', context)
 
+
 def add_food(request):
     """ Add a product to the store """
-    form = FoodForm()
+    if request.method == 'POST':
+        form = FoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_food'))
+        else:
+            messages.error(
+                request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = FoodForm()
+
     template = 'orders/add_food.html'
     context = {
         'form': form,
