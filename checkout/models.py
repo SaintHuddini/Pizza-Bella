6c4d53924892fc5126dcac5fa1dc96d21a414540
SaintHuddini <Salah1799@hotmail.com>
2020-07-25 12:55:20 +0000

    fixed checkout bugg that shows delivery = /bin/bash and the order of the orders in the profile

--- checkout/models.py
+++ checkout/models.py
@@ -24,10 +24,10 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,
-                                        null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,
+                                        null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
     original_cart = models.TextField(null=False, blank=False, default='')
@@ -47,7 +47,7 @@ class Order(models.Model):
         """
         self.order_total = self.lineitems.aggregate(
             Sum('lineitem_total'))['lineitem_total__sum'] or 0
-        if self.order_total < 0:
+        if self.order_total > 0:
             self.delivery_cost = 2
         else:
             self.delivery_cost = 0
