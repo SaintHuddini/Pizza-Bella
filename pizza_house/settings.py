9b5c06f8f08bec8660f3ce826662fbab3b041e95
SaintHuddini <Salah1799@hotmail.com>
2020-07-31 02:50:21 +0000

    Updated allowed hosts

--- pizza_house/settings.py
+++ pizza_house/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://pizzahouse163.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['pizzahouse163.herokuapp.com', 'localhost']
 
 
 # Application definition
