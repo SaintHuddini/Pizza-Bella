527d62abbb13d1fb11a59ec5ba5b6647cac80cf6
SaintHuddini <Salah1799@hotmail.com>
2020-08-01 15:07:11 +0000

    settings.py bug email updated and fixed

--- pizza_house/settings.py
+++ pizza_house/settings.py
@@ -190,9 +190,10 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'pizzahouse@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    SERVER_EMAIL = os.environ.get('EMAIL_HOST_USER')
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
